03978581993bd1d44a21b893a8db003fcafff52b
HMason002 <10896788@uvu.edu>
2026-09-01 13:25:04 -0600

    test after rename

---
+++ Class Notes Sep 1.R
@@ -0,0 +1,28 @@
+getwd()
+getwd()
+read.csv('Data/')
+read.csv('Data/1620_scores.csv')
+# practice note #
+
+getwd
+getwd()
+
+csvfile = read.csv('Data/1620_scores.csv')
+
+obj1 = 2
+is.character(obj1)
+is.numeric(obj1)
+
+as.character(obj1)
+
+is.numeric(obj1)
+
+obj1 = as.character(obj1)
+
+is.numeric(obj1)
+
+vec = c(1, 2, 3)
+vec
+vec + 1
+
+
